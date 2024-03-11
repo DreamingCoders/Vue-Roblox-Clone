7d8af0eb7e898fb43736bb39b104ede2eb7fd370
Maui <61954544+DreamingCoders@users.noreply.github.com>
2024-03-11 01:07:29 -0700

    Update page.php
    Beautified the index page.

--- page.php
+++ page.php
@@ -6,7 +6,7 @@ if(!isset($isConfig)){
     // If there isn't a config detected we can add custom overrides here, generally it will run false flags for now.
 }
 if(!isset($siteName)){
-$siteName = "One Page App"; // If you dont want to load header or config, preferrably used for skeleton API's or single static data.
+$siteName = "One Page"; // If you dont want to load header or config, preferrably used for skeleton API's or single static data.
 }
 
 $page = $_GET['page'];
@@ -19,8 +19,29 @@ switch ($page) {
     case 'defaultPage':
         echo '
         <div class="card">
-        <div class="card-header bg-primary text-white"><h2>Welcome to '.$siteName.'!</h2></div>
-        <div class="card-body"><a class="btn btn-success text-white">Login</a> <a class="btn btn-primary text-white">Register</a></div>
+        <div class="card-header bg-primary text-white">
+            <h2>Welcome to '.$siteName.'!</h2>
+        </div>
+        <div class="card-body">
+        <div class="row">
+            <div class="col">
+            <h2>You are now playing '.$siteName.'!</h2>
+            '.$siteName.' is a One Paged Laravel alternative for sandbox/roblox like websites.
+            everything is loading into one page and templated amazingly with raw PHP. We
+            give similar features to Laravel but without having to use their bloated regex and
+            allow you to code full PHP without implicitly needing new routing, you can add pages
+            via get requests javascript routes and database logics, much more to offer than your
+            conventional framework!
+            <h2>Get Started</h2>
+            <h4>You will be the 1300th User!</h4>
+            <p>If you register, or you can login with either of the buttons below.</p>
+                    <a class="btn btn-success text-white">Login</a> 
+                    <a class="btn btn-primary text-white">Register</a>
+            </div>
+            <div class="col">
+                <img src=\'https://nova-cubes.com/storage/avatars/uma/2.png\'">
+            </div>
+        </div>
         </div>
         <br><br>';
         break;
