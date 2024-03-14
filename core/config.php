7ec90ab62e1e7fe6e822d1bdf9d55a8f21dad38e
Maui <61954544+DreamingCoders@users.noreply.github.com>
2024-03-14 08:27:34 -0700

    Update config.php
    Added ajax apis, error reporting and more.

--- core/config.php
+++ core/config.php
@@ -1,6 +1,11 @@
 <?php
 $isConfig = true;
-
+$errorsEnabled = true;
+if(isset($errorsEnabled){
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+}
 if(isset($_GET['loadScripts']) || $isConfig == true){
 <?php
 $host = 'localhost'; // You normally will leave this the same unless you're doing Remote SQL, in this case we are using MySQL/PDO.
@@ -17,8 +22,23 @@ try {
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-?>
+$ajax = "enabled";
+if(isset($ajax)){
+// AJAX
+header('Content-type:application/json;charset=utf-8');
 
+$jsonStr ='{
+	"main": {
+		"title": "Hello",
+		"desc": "World!"
+	}
+}';
+
+//$json = json_encode($jsonStr, JSON_PRETTY_PRINT);
+//echo json_decode($json);
+
+echo $jsonStr;
     //require('vendor/javascript/cards.js');
+}
 }
 ?>
