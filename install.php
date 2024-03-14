7fd18535172318caa989b98c15ba1cea8c9c360d
Maui <61954544+DreamingCoders@users.noreply.github.com>
2024-03-14 08:32:14 -0700

    Update install.php
    Added the finalize portion

--- install.php
+++ install.php
@@ -518,4 +518,18 @@ $('#action-button').click(function() {
 </body>
 </html>
   <?php
+  if(isset($_GET['finalize.php')){
+include_once("avatar.php");
+
+//send an email to the owner and ANS creator
+
+  // send a private webhook for additional security
+
+echo"Your AvatarNetworkScript installation returned successful!";
+echo"Your data:" . $obj . ".";
+echo"Server name: ".$serverName."";
+echo"Admin account: ".$adminUser."";
+
+echo"<a href="/">View your all new website!</a>";
+  }
 ?>
