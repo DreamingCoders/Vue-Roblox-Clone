d5d89db988f46aad1f61a2a567bfcf8736117f09
Maui <61954544+DreamingCoders@users.noreply.github.com>
2024-03-14 07:40:31 -0700

    Moved footer.php
    Made it display.

--- html/footer.php
+++ html/footer.php
@@ -1,1 +1,7 @@
-<!-- Add the footer page -->
+<div class="card">
+    <div class="card-header bg-primary text-white">
+Copyright &copy; <?=date("Y")?> <?=$siteName?> All Rights Reserved.
+</div>
+<div class="card-body">
+    Links
+</div>
