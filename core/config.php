da71b5024923666432ace071626fa0ae038b18b4
Maui <61954544+DreamingCoders@users.noreply.github.com>
2024-04-10 16:07:57 -0700

    Update config.php
    Added session control & readability improvements

--- core/config.php
+++ core/config.php
@@ -1,11 +1,22 @@
 <?php
+// config
 $isConfig = true;
+// error logging
 $errorsEnabled = true;
 if(isset($errorsEnabled){
     ini_set('display_errors', 1); 
     ini_set('display_startup_errors', 1); 
     error_reporting(E_ALL);
 }
+// sessions
+if(!isset($_SESSION['vue-enc-login-userid-a92bc426a']) || !isset($_SESSION['your-enc-subdmn'])){
+	session_start(); // this actually prevents hectic logs by validating session is available.
+}
+if(isset($_SESSION['vue-enc-login-userid-a92bc426a'])){ // in case we ever need a seperate session this prevents subdomains logging in other users.
+	$sessionName = 'vue-enc-login-userid-a92bc426a'; // repeat this
+	$LocalUserId = $sessionName; // this hard to access static session is now calling all user ids.
+}
+// db connections and checksums
 if(isset($_GET['loadScripts']) || $isConfig == true){
 <?php
 $host = 'localhost'; // You normally will leave this the same unless you're doing Remote SQL, in this case we are using MySQL/PDO.
